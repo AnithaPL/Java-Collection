5c96b43cbf6ef6fc20c89c18bababf26baf4cf56
AnithaPL <anitha02pl@gmail.com>
2024-07-14 14:18:26 +0530

    ex1

---
+++ src/arrayList/Exercise1.java
@@ -0,0 +1,21 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise1 {
+
+	
+	//Create a new array list, add some elements and print out the collection
+	
+	public static void main(String[] args) {
+		List<String> list_Strings = new ArrayList<String>();
+		  list_Strings.add("Red");
+		  list_Strings.add("Green");
+		  list_Strings.add("Orange");
+		  list_Strings.add("White");
+		  list_Strings.add("Black");
+		  System.out.println(list_Strings);
+	}
+
+}
